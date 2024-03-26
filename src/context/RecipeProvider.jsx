b68970dc815a9a7fde9b1290c3f2d936f6a7845a
emre <mrtuncer34@gmail.com>
2024-03-26 11:32:36 +0300

    recipeprovider page finished

---
+++ src/context/RecipeProvider.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const APP_ID = "bfbb3efc";
+const APP_KEY = "43faeee790f26cd82b28050d3031619d";
+
+const RecipeProvider = () => {
+
+const [name, setName]=useState("")
+const [password, setPassword]=useState("")
+    
+
+
+
+  return (
+    <div>RecipeProvider</div>
+  )
+}
+
+export default RecipeProvider
